ba3734e4e055c0c834f21848c4238f3a13157530
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-05-08 08:27:58 +0200

    Use requests json decoder

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
-import json
 
 workshop_list_response = requests.get('https://modworkshop.net/mws/api/modsapi.php?page=1&func=mods&count_total=1&sort=&gid=752&_=1588918349135')
 workshop_list_response.raise_for_status()
 
-workshop_list_json = json.loads(workshop_list_response.text)
+workshop_list_json = workshop_list_response.json()
 workshop_list = workshop_list_json['content']
 
 for mod in workshop_list:
