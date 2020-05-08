e71f1df51bf135ca42f3ffc9fac0d6601494a4a5
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-05-08 08:40:15 +0200

    Add function for downloading mod

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import requests
 
+def download_mod(did, fid):
+	args = {
+		'did': did,
+		'fid': fid,
+		'func': 'download',
+	}
+
+	response = requests.post('https://modworkshop.net/mws/api/modsapi.php', data = args)
+	response.raise_for_status()
+
+	return response
+
 workshop_list_response = requests.get('https://modworkshop.net/mws/api/modsapi.php?page=1&func=mods&count_total=1&sort=&gid=752&_=1588918349135')
 workshop_list_response.raise_for_status()
 
